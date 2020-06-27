6b5270388b32ace84a5f0f88c38d063c17368a2f
WaDo92 <charles.vanhamme@gmail.com>
2020-06-27 14:12:27 +0200

    NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import history from './history';
 import './App.css';
 import routes from './routes/routeConfigurations';
 import PrivateRoute from './components/Auth/PrivateRoute';
+import NavBar from './components/Navigation/NabBar';
 
 const generateRoutes = () => routes.map(route => route.private !== false ? <PrivateRoute {...route} /> : <Route {...route} />);
 
 function App() {
   return (
       <Router history={history}>
+
+        <NavBar />
         <Switch>
         
           {generateRoutes()}
