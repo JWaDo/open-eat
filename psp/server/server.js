b299075392e8f1105ae53166cef5a2d734d7b755
Ilan Ducret <ilsduc@ilsduc.fr>
2020-06-29 12:44:11 +0200

    [add] PSP architecture

---
+++ psp/server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// PSP Endpoint listening for POST request on /process_payment
+app.post('/process_payment', (req, res) => {
+
+  setTimeout(() => {
+    // Waiting 10 seconds before send a response
+    // Do card verification and other things must be handle by PSP here
+    res.status(200).json({ success: true });
+
+  }, 10 * 1000);
+
+  
+});
+
+const serverPort = 9000;
+// Start server
+const server = app.listen(serverPort);
+
+export default app;
