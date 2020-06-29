582f8c37b59b424cfb87623d3fbbe7389efdd604
Ilan Ducret <ilsduc@ilsduc.fr>
2020-06-29 14:32:00 +0200

    [add] PSP communication

--- psp/server/server.js
+++ psp/server/server.js
@@ -11,9 +11,12 @@ app.use(cors());
 // PSP Endpoint listening for POST request on /process_payment
 app.post('/process_payment', (req, res) => {
 
+  console.log('PSP received a payment request: ', req.body);
+  
   setTimeout(() => {
     // Waiting 10 seconds before send a response
     // Do card verification and other things must be handle by PSP here
+    console.log('PSP has finished verifications');
     res.status(200).json({ success: true });
 
   }, 10 * 1000);
