a5496ba29ad26b8eb9a0e8c0fc4fe3b5dcba3904
WaDo92 <charles.vanhamme@gmail.com>
2020-07-09 12:50:24 +0200

    Removed .gitIgnore

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import { fireAuth } from '../../firebase/config';
 import { navigate } from '../../routes';
 import { useSnackbar } from 'notistack';
-import { Typography, Link } from '@material-ui/core';
+import { Link, Box, Paper, TextField, Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    box: {
+        height: "100vh",
+        width: "100vw",
+    }
+}))
 
 function LoginForm() {
     const [user, setUser] = useState({});
     const { enqueueSnackbar } = useSnackbar();
+    const classes = useStyles();
     
     const handleChange = (e) => {
         setUser({
@@ -17,6 +26,7 @@ function LoginForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(user.password);
         fireAuth.signInWithEmailAndPassword(user.email, user.password)
             .then(user => {
                 console.log("connected");
@@ -30,25 +40,66 @@ function LoginForm() {
     }
 
     return (
-        <div>
-            LoginForm
-            <form method="post" onSubmit={handleSubmit}>
-                <input type="text" name="email" onChange={handleChange}/>
-                <input type="text"name="password" onChange={handleChange}/>
-                <button type="submit">Login</button>
-            </form>
-            <Link
-                component="button"
-                variant="body"
-                onClick={(e) => {
-                    e.preventDefault();
-                    navigate.push("RegisterPage");
-                }}
+        <Box 
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            className={classes.box}
+        >
+            <Paper elevation={3} >
+                
+                <Grid 
+                    container
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={1}
+                    
                 >
-                Not registered yet ? Come here
-            </Link>
-        </div>
+                    <form>
+                        <Grid item xs={12}>
+                            <TextField 
+                                name="email"
+                                label="Email" 
+                                onChange={handleChange}
+                            />
+                        </Grid>   
+                        <Grid item xs={12}>
+                            <TextField 
+                                name="password"
+                                label="Password" 
+                                type="password"
+                                onChange={handleChange}
+                            />
+                        </Grid>   
+                        <Grid item xs={12}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={handleSubmit}
+                            >
+                                Login
+                            </Button>
+                        </Grid>   
+                    </form>
+
+                    <Grid item xs={12}>
+                        <Link
+                            component="button"
+                            variant="body"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                navigate.push("RegisterPage");
+                            }}
+                            >
+                            Not registered yet ? Come here
+                        </Link>
+                    </Grid>
+                </Grid>
+
+            </Paper>
+        </Box>
     )
 }
 
-export default LoginForm
+export default LoginForm;
---
+++ client/src/firebase/collections/Users.js
@@ -0,0 +1,4 @@
+import { firestore } from '../config';
+
+export default firestore.collection('users');
+
